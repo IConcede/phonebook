beff2f1aec8e7acd2107c8b2a9205c4fda56a7dd
Max Jin <max.jin@mail.utoronto.ca>
2022-02-03 15:21:21 -0500

    added database

--- index.js
+++ index.js
@@ -11,36 +11,15 @@ app.use(express.static('build'));
 app.use(cors());
 app.use(morgan('tiny'));
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     Person.find({}).then(result => {
         response.json(result);
-    });
+    })
+    .catch(error => next(error));
 });
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
     Person.find({}).then(result => {
         const time = new Date();
         const info = 
@@ -50,6 +29,7 @@ app.get('/info', (request, response) => {
             </div>`;
         response.send(info);
     })
+    .catch(error => next(error));
 });
 
 app.get('/api/persons/:id', (request, response) => {
@@ -57,15 +37,11 @@ app.get('/api/persons/:id', (request, response) => {
 
     Person.findById(id).then(result => {
         response.json(result);
-    });
+    })
+    .catch(error => next(error));
 });
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    const id = Number(request.params.id);
-    persons = persons.filter(person => person.id !== id);
-
-    response.status(204).end()
-
     Person.findByIdAndRemove(request.params.id)
     .then(result => {
         response.status(204).end();
@@ -73,23 +49,17 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error));
 });
 
-const generateID = () => {
-    return Math.floor(Math.floor(Math.random() * 1000000));
-}
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response,next) => {
     const body = request.body;
-    const nameExists = persons.find(person => person.name === body.name);
 
-    if(!body.name || !body.number || nameExists) {
+    if(!body.name || !body.number) {
         let message;
 
         if(!body.name) {
             message = 'name is missing'
         } else if(!body.number) {
             message = 'number is missing'
-        } else if(nameExists) {
-            message = 'name must be unique'
         }
 
         return(response.status(400).json({
@@ -98,21 +68,53 @@ app.post('/api/persons', (request, response) => {
     }
 
     const person = new Person({
-        id: generateID(),
         name: body.name,
         number: body.number,
     });
 
     person.save().then(result => {
         response.json(result);
-    });
+    })
+    .catch(error => next(error));
 });
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body;
+
+    const person = {
+      name: body.name,
+      number: body.number,
+    };
+  
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        console.log(updatedPerson)
+        response.json(updatedPerson)
+      })
+      .catch(error => {
+          console.log(request.body);
+          next(error)})
+  })
+
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+    response.status(404).send({ error: 'unknown endpoint' });
   }
   
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+  
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' });
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+  
+    next(error);
+  }
+  
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
